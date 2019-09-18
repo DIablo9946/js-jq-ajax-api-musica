60cb51b0d57fcbf38399f6812a77f249b64c669b
Diablo9946 <diabloch9946@gmail.com>
2019-09-18 06:35:27 +0200

    Temlate is ready to be implemented.

--- action.js
+++ action.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
 
 
-  var source   = document.getElementById("entry-template").innerHTML;
+  var source   = $("#template").html();
   var template = Handlebars.compile(source);
 
 
